172e74e026de8ede52d0a9a4840a4c90d02aeb43
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-05-12 16:59:11 +0300

    Add Errors Handlers

--- app/action.tsx
+++ app/action.tsx
@@ -3,12 +3,16 @@
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
 export const fetchData = async (page: number) => {
-  const response = await fetch(
-    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
-  );
-  const data = await response.json();
-  // console.log(data);
-  return data.map((item: AnimeProp, index: number) => (
-    <AnimeCard key={item.id} anime={item} index={index} />
-  ));
+  try {
+    const response = await fetch(
+        `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+    );
+    const data = await response.json();
+    // console.log(data);
+    return data.map((item: AnimeProp, index: number) => (
+        <AnimeCard key={item.id} anime={item} index={index} />
+    ));
+  }catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
